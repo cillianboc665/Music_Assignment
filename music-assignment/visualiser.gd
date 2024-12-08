26b6e857c9ed35f9f9ae9d0dca7633f50d9afc1e
cillianboc665 <c24377323@mytudublin.ie>
2024-12-08 23:49:58 +0000

    donezo

--- music-assignment/visualiser.gd
+++ music-assignment/visualiser.gd
@@ -3,26 +3,50 @@ extends Control
 	
 func _on_button_button_down() -> void:
 	if $AudioStreamPlayer2D2.playing:
+		$Pause.text = "Pause Song"
 		$Button.text = "Play Song"
 		$AudioStreamPlayer2D2.stop()
 	else:
-#		AudioServer.remove_bus_effect(0, 0)
-		AudioServer.set_bus_effect_enabled(0,0, false)
+		$Pause.text = "Pause Song"
 		$Button.text = "Stop Song"
 		$AudioStreamPlayer2D2.play()
 	pass # Replace with function body.
+	
+	
+
+var music_pos = 0.0
+
+func _on_pause_button_down() -> void:
+#	var temp = $AudioStreamPlayer2D2.get_playback_position()
+	if $AudioStreamPlayer2D2.playing:
+		music_pos = $AudioStreamPlayer2D2.get_playback_position()
+		$AudioStreamPlayer2D2.stop()
+		$Pause.text = "Unpause Song"
+	elif !$AudioStreamPlayer2D2.playing:
+		$AudioStreamPlayer2D2.play(music_pos)
+		$Pause.text = "Pause Song"
+	pass # Replace with function body.
+	
+	
+	
+func _on_volume_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(0, linear_to_db(value))
+	pass # Replace with function body.
+	
+	
 	
 func _on_pitch_scale_value_changed(value: float) -> void:
 	$AudioStreamPlayer2D2.pitch_scale = value
 	pass # Replace with function body.
 
+
+
 func _on_button_ph_button_down() -> void:
-	if $ButtonPh.text == "Phaser On":
-		$ButtonPh.text = "Phaser Off"
+	if $ButtonPh.text == "Phaser: On":
+		$ButtonPh.text = "Phaser: Off"
 		AudioServer.set_bus_effect_enabled(0,0, false)
-#		AudioServer.remove_bus_effect(0, 0)
-	elif $ButtonPh.text == "Phaser Off":
-		$ButtonPh.text = "Phaser On"
+	elif $ButtonPh.text == "Phaser: Off":
+		$ButtonPh.text = "Phaser: On"
 		AudioServer.set_bus_effect_enabled(0,0, true)
 	pass # Replace with function body.
 
@@ -30,6 +54,143 @@ func _on_phaser_value_changed(value: float) -> void:
 	var AudioEffectPhaser = AudioServer.get_bus_effect(0, 0)
 	AudioEffectPhaser.rate_hz = value
 	pass
+
+
+
+func _on_button_rev_button_down() -> void:
+	if $ButtonRev.text == "Reverb: On":
+		$ButtonRev.text = "Reverb: Off"
+		AudioServer.set_bus_effect_enabled(0,1, false)
+	elif $ButtonRev.text == "Reverb: Off":
+		$ButtonRev.text = "Reverb: On"
+		AudioServer.set_bus_effect_enabled(0,1, true)
+	pass # Replace with function body.
+
+
+
+
+func _on_button_hpf_button_down() -> void:
+	if $ButtonHPF.text == "High Pass Filter: On":
+		$ButtonHPF.text = "High Pass Filter: Off"
+		AudioServer.set_bus_effect_enabled(0,2, false)
+	elif $ButtonHPF.text == "High Pass Filter: Off":
+		$ButtonHPF.text = "High Pass Filter: On"
+		AudioServer.set_bus_effect_enabled(0,2, true)
+	pass # Replace with function body.
+
+
+
+func _on_button_lpf_button_down() -> void:
+	if $ButtonLPF.text == "Low Pass Filter: On":
+		$ButtonLPF.text = "Low Pass Filter: Off"
+		AudioServer.set_bus_effect_enabled(0,3, false)
+	elif $ButtonLPF.text == "Low Pass Filter: Off":
+		$ButtonLPF.text = "Low Pass Filter: On"
+		AudioServer.set_bus_effect_enabled(0,3, true)
+	pass # Replace with function body.
+	
+	
+
+func _on_button_ch_button_down() -> void:
+	if $ButtonCh.text == "Chorus: On":
+		$ButtonCh.text = "Chorus: Off"
+		AudioServer.set_bus_effect_enabled(0,4, false)
+	elif $ButtonCh.text == "Chorus: Off":
+		$ButtonCh.text = "Chorus: On"
+		AudioServer.set_bus_effect_enabled(0,4, true)
+	pass # Replace with function body.
+		
+	
+	
+func _on_test_button_down() -> void:
+	if $maccies.playing:
+		$macciesbutton.text = "McDonald's"
+		$maccies.stop()
+	else:
+		$maccies.play()
+	pass # Replace with function body.
+	
+	
+
+func _on_screech_b_button_down() -> void:
+	if $screech.playing:
+		$ScreechB.text = "Hell's Kitchen Screech"
+		$screech.stop()
+	else:
+		$screech.play()
+	pass # Replace with function body.
+
+
+
+func _on_pipe_b_button_down() -> void:
+	if $pipe.playing:
+		$PipeB.text = "Metal Pipe"
+		$pipe.stop()
+	else:
+		$pipe.play()
+	pass # Replace with function body.
+
+
+
+func _on_clap_1b_button_down() -> void:
+	if $clap1.playing:
+		$clap1B.text = "Single Clap"
+		$clap1.stop()
+	else:
+		$clap1.play()
+	pass # Replace with function body.
+
+
+
+func _on_clap_2b_button_down() -> void:
+	if $clap2.playing:
+		$clap2B.text = "Multiple Claps"
+		$clap2.stop()
+	else:
+		$clap2.play()
+	pass # Replace with function body.
+
+
+
+func _on_snare_b_button_down() -> void:
+	if $snare.playing:
+		$snareB.text = "Snare"
+		$snare.stop()
+	else:
+		$snare.play()
+	pass # Replace with function body.
+
+
+
+func _on_hihat_b_button_down() -> void:
+	if $hihat.playing:
+		$hihatB.text = "Hi-Hat"
+		$hihat.stop()
+	else:
+		$hihat.play()
+	pass # Replace with function body.
+
+
+
+func _on_kick_1b_button_down() -> void:
+	if $kick1.playing:
+		$kick1B.text = "Reverse Bass Kick"
+		$kick1.stop()
+	else:
+		$kick1.play()
+	pass # Replace with function body.
+
+
+
+func _on_kick_2b_button_down() -> void:
+	if $kick2.playing:
+		$kick2B.text = "Bass Kick"
+		$kick2.stop()
+	else:
+		$kick2.play()
+	pass # Replace with function body.
+
+
 
 
 
@@ -62,6 +223,11 @@ const MIN_DB = 70
 func _ready() -> void:
 	bottomleftArray.reverse()
 	topleftArray.reverse()
+	AudioServer.set_bus_effect_enabled(0,0, false)
+	AudioServer.set_bus_effect_enabled(0,1, false)
+	AudioServer.set_bus_effect_enabled(0,2, false)
+	AudioServer.set_bus_effect_enabled(0,3, false)
+	AudioServer.set_bus_effect_enabled(0,4, false)
 	pass # Replace with function body.
 
 	
